e681e66a91cce630cc9166a3eccc083b65aacd6a
Liza Star <lizadstar18@gmail.com>
2020-06-22 19:04:17 -0500

    routing works- built navbar on this branch in order to fully test that routing works!

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -1,22 +1,75 @@
 import React from 'react';
+import { NavLink as RRNavLink } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import PropTypes from 'prop-types';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool.isRequired,
+  }
+
+  state = {
+    isOpen: false,
+  }
+
 
   logMeOut = (e) => {
     e.preventDefault();
     firebase.auth().signOut();
   }
-
+  
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpenisOpen });
+  }
+  
   render() {
+    const { isOpen } = this.state;
+
+    const buildNavbar = () => {
+      const { authed } = this.props;
+      if (authed) {
+        return (
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/home'>Home</NavLink>
+             </NavItem>
+             <NavItem>
+               <NavLink tag={RRNavLink} to='/milestone/new'>New Milestone</NavLink>
+             </NavItem>
+             <NavItem>
+               <NavLink tag={RRNavLink} to='/child/new'>Add Child</NavLink>
+             </NavItem>
+               <NavItem>
+             <NavLink onClick={this.logMeOut}>Logout</NavLink>
+                </NavItem>
+          </Nav>
+        );
+      }
+      return <Nav className="ml-auto" navbar></Nav>;
+    };
     return (
       <div className="MyNavbar">
-      <h1> BusyBaby </h1>
-      <button className="btn btn-danger" onClick={this.logMeOut}> Log Out</button>
-    </div>
-    ) 
+         <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">BusyBaby</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={isOpen} navbar>
+          {buildNavbar()}
+          </Collapse>
+        </Navbar>
+      </div>
+    );   
   }
 }
+
 export default MyNavbar;
